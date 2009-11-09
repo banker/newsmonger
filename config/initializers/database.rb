1ca773ed4b6d10112f5e0f0dc5a8bb619a8ba84c
Kyle Banker <kylebanker@gmail.com>
2009-11-09 17:58:24 -0500

    Removed MongoHQ data

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -1,13 +1,15 @@
 include MongoMapper
 
 # Settings for MongoHQ
-if ['production', 'development'].include?(Rails.env)
-  MongoMapper.connection = Mongo::Connection.new('db.mongohq.com')
-  MongoMapper.database = 'brooklyn'
-  MongoMapper.database.authenticate('kyle', 'secret')
-else
-  MongoMapper.database = "news-#{Rails.env}"
-end
+# if ['production', 'development'].include?(Rails.env)
+#  MongoMapper.connection = Mongo::Connection.new('db.mongohq.com')
+#  MongoMapper.database = 'brooklyn'
+#  MongoMapper.database.authenticate('kyle', 'secret')
+# else
+#  MongoMapper.database = "news-#{Rails.env}"
+# end
+
+MongoMapper.database = "news-#{Rails.env}"
 
 # It's also possible to define indexes in the the model itself; however,
 # a few issues are being worked out still. This is a temporary solution.
